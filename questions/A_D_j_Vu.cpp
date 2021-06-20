af154ef1d3dc4ad05ab5abe0c0d7b14a6e9c7948
VinitAwale <awale.vinit@gmail.com>
2021-06-20 20:24:00 +0530

    Comeback

---
+++ questions/A_D_j_Vu.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int flag = 0;
+        string s;
+        cin>>s;
+        for(int i = 0; i<s.length() ; i++)
+        {
+            if([s.length() - i-1] != 'a')
+            {
+                flag = 1;
+                s.insert(i , "a");
+                break;
+            }
+
+        }
+
+        if(flag == 1)
+        {
+            cout<<"YES"<<endl;
+            cout<<s<<endl;
+        }
+        else
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
--- questions/a.cpp
+++ questions/a.cpp
@@ -1,14 +1,22 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
+#define int long long
+#define endl "\n"
+
+int32_t main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int t;
     cin>>t;
     while (t--)
     {
-        cout<<"Hello world"<<endl;
+        
     }
+    
+    
     
     return 0;
 }
