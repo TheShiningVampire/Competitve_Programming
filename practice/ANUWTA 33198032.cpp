5e720cec53f23384c2e959d48335b146a63475ac
Vinit Awale <awale.vinit@gmail.com>
2022-02-01 23:56:44 +0530

    "ANUWTA 33198032.cpp": update

--- practice/ANUWTA 33198032.cpp
+++ practice/ANUWTA 33198032.cpp
@@ -1,16 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	int t;
-	cin>>t;
-	while(t--)
+	cin >> t;
+	while (t--)
 	{
 		long long int n;
-		cin>>n;
-		cout<<(n*(n+3))/2<<endl;
-		
-
-	}	
+		cin >> n;
+		cout << (n * (n + 3)) / 2 << endl;
+	}
 	return 0;
 }
