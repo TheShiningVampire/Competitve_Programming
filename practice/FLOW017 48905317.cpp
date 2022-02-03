06b1e7713bac482010ae73df0e6aa2c74e5783d8
Vinit Awale <awale.vinit@gmail.com>
2022-02-03 23:52:57 +0530

    "practice/FLOW017 48905317.cpp": update

--- practice/FLOW017 48905317.cpp
+++ practice/FLOW017 48905317.cpp
@@ -9,16 +9,16 @@ int32_t main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
         vector<int> a(3);
-        for(int i = 0; i<3 ; i++)
+        for (int i = 0; i < 3; i++)
         {
-            cin>>a[i];
+            cin >> a[i];
         }
-        sort(a.begin(),a.end());
-        cout<<a.at(1)<<endl;
+        sort(a.begin(), a.end());
+        cout << a.at(1) << endl;
     }
 
     return 0;
