785a232fe06ab84f9905bf4e9157a55f7caac1ce
Vinit Awale <awale.vinit@gmail.com>
2022-02-03 23:52:03 +0530

    practice/Forgotten_Language: udpate

--- practice/Forgotten_Language.cpp
+++ practice/Forgotten_Language.cpp
@@ -9,36 +9,35 @@ int32_t main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
-    cin>>t;
-    while (t--){
-        int n,k;
-        cin>>n>>k;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
         vector<string> dict(n);
-        for (int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
-            cin>>dict[i];
+            cin >> dict[i];
         }
         while (k--)
         {
             int a;
-            cin>>a;
-            while(a--)
+            cin >> a;
+            while (a--)
             {
                 string k;
-                cin>>k;
-                
-                if (find(dict.begin(),dict.end(),k)== dict.end())
+                cin >> k;
+
+                if (find(dict.begin(), dict.end(), k) == dict.end())
                 {
-                    cout<<"NO ";
+                    cout << "NO ";
                     break;
                 }
             }
-            cout<<"YES ";
+            cout << "YES ";
         }
-        cout<<endl;
-
+        cout << endl;
     }
-
 
     return 0;
 }
