310500995e667eda532674eb21b9d0e1bdcefbc8
Vinit Awale <awale.vinit@gmail.com>
2022-02-03 23:52:39 +0530

    "practice/FSQRT 48905234.cpp": update

--- practice/FSQRT 48905234.cpp
+++ practice/FSQRT 48905234.cpp
@@ -9,12 +9,12 @@ int32_t main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
         int a;
-        cin>>a;
-        cout<<round(sqrt(a))<<endl;
+        cin >> a;
+        cout << round(sqrt(a)) << endl;
     }
 
     return 0;
