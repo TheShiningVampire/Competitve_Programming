bbc023ac55a6e99e18552e0f435e20695fb416d9
Vinit Awale <awale.vinit@gmail.com>
2022-02-05 23:22:57 +0530

    "ATTIC 33380701.cpp": update

--- practice/ATTIC 33380701.cpp
+++ practice/ATTIC 33380701.cpp
@@ -9,42 +9,35 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         string s;
-        cin>>s;
-        int maxc = 0  , i=0 , ans =0;
-        while(i<s.length())
+        cin >> s;
+        int maxc = 0, i = 0, ans = 0;
+        while (i < s.length())
         {
-            if(s[i] == '.')
+            if (s[i] == '.')
             {
                 int count = 1;
-                while(s[i] !='#')
+                while (s[i] != '#')
                 {
                     count++;
                     i++;
                 }
 
-                if(count>maxc)
+                if (count > maxc)
                 {
                     maxc = count;
                     ans++;
                 }
             }
 
-            
-
             i++;
         }
 
-        cout<<ans<<endl;
-
+        cout << ans << endl;
     }
-
-
-
-
 
     return 0;
 }
