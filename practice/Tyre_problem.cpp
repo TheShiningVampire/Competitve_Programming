f350de9c587cb6dfcf649b2a45ea68840d41f90d
Vinit Awale <awale.vinit@gmail.com>
2022-03-14 23:54:52 +0530

    Tyre_problem: create

---
+++ practice/Tyre_problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        cout<< 2*n + 4*m <<endl;
+    }
+
+
+    return 0;
+}
