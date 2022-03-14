421975cd9f5b53b9e1b5c8dc2ce8772aaf8a8005
Vinit Awale <awale.vinit@gmail.com>
2022-03-14 23:54:45 +0530

    Chef_and_Instant_Noodles: create

---
+++ practice/Chef_and_Instant_Noodles.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin>>n>>m;
+    cout<<n*m<<endl;
+
+
+    return 0;
+}
