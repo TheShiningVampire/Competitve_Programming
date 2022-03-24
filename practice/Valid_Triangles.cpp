202fa78bbceb4b4a952a8e5405153702bd008134
Vinit Awale <awale.vinit@gmail.com>
2022-03-24 23:54:10 +0530

    update Valid_Triangles.cpp

---
+++ practice/Valid_Triangles.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+int32_t main()
+{
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        vector<int> sides(3);
+        for (int i = 0; i < 3; i++)
+        {
+            cin>>sides[i];
+        }
+        if(sides[0] + sides[1] + sides[2] == 180)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
