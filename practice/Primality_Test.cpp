8d4babbc243c58bb79eceec153b2b6693d3949c0
Vinit Awale <awale.vinit@gmail.com>
2022-05-20 00:36:22 +0530

    update 5 files, create 4 files, delete 1 file and copy 1 file

--- practice/Primality_Test.cpp
+++ practice/Primality_Test.cpp
@@ -8,32 +8,39 @@ int32_t main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    vector<int> nprime(100001 , 0);
-    nprime[0] = 1;
-    nprime[1] = 1;
-
-    for (int i = 0; i*i < 100001 ; i++)
+    vector<int> comp(1000001,0);
+    comp[0] = 1;
+    comp[1] = 1;
+    
+    for(int i = 2; i< 1000; i++)
     {
-        if(nprime[i] ==  0)
+        if (comp[i] == 0)
         {
-            for (int j = 2*i; j<100001 ; j+=i)
+            for(int j = 2*i; j<1000001; j+=i)
             {
-                nprime[j] =1;
+                comp[j] = 1;
             }
         }
+    }
 
-    }    
 
     int t;
     cin>>t;
-    while (t--)
+    while(t--)
     {
         int n;
         cin>>n;
-        cout<<(nprime[n] == 1 ? "no":"yes" )<<endl;
+        if (comp[n] == 0)
+        {
+            cout<<"yes"<<endl;
+        }  
+        else
+        {
+            cout<<"no"<<endl;
+        }
+
     }
+
 
     return 0;
 }
-
---
+++ practice/temp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout<<(5 == 1?"Yes" : "No");
+    cout<<endl;
+
+    return 0;
+}
